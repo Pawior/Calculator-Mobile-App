d5288719d0aa67a2e00559f68534a8d19f52eee7
Pawior <pawiorx@gmail.com>
2022-11-01 13:23:40 +0100

    calculator ready on portrait view

--- CalcApp.js
+++ CalcApp.js
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import React, { useState } from "react";
 import NumberComp from "./components/NumberComp";
 
 let fCol = ["1", "4", "7", "."];
@@ -7,20 +7,191 @@ let sCol = ["2", "5", "8", "0"];
 let tCol = ["3", "6", "9", "="];
 let lCol = ["C", "/", "*", "-", "+"];
 
+let fLCol = ["1", "4", "7", "."];
+let sLCol = ["2", "5", "8", "0"];
+let tLCol = ["3", "6", "9", "="];
+let nLCol = ["Sqrt", "Pow", "Sin", "Cos"];
+let lLCol = ["Del", "C", "/", "*", "-", "+"];
+
 const CalcApp = () => {
-  return (
+  const [inputedNums, setInputedNums] = useState([]);
+  const [result, setResult] = useState();
+  const [orientation, setOrientation] = useState(true);
+
+  const isPortrait = () => {
+    let dim = Dimensions.get("screen");
+    console.log("robie i zwaracam " + dim);
+    return dim.height >= dim.width;
+  };
+
+  Dimensions.addEventListener("change", () => {
+    setOrientation(isPortrait());
+  });
+
+  const makeCalculation = () => {
+    console.log("licze");
+    console.log(inputedNums.join(""));
+    console.log(eval(inputedNums.join("")));
+    setResult(eval(inputedNums.join("")));
+  };
+  const clearCalculation = () => {
+    setInputedNums([]);
+    setResult();
+  };
+  const updateCalcsBoard = (numb) => {
+    console.log("dodaje numa");
+    if (numb != "C" && numb != "=") {
+      // console.log("test");
+      setInputedNums([...inputedNums, numb]);
+    } else if (numb == "=") {
+      makeCalculation();
+    } else if (numb == "C") {
+      clearCalculation();
+    }
+  };
+
+  return orientation ? (
     <View style={styles.container}>
-      <View style={styles.mathBoard}></View>
+      <View style={styles.mathBoard}>
+        {/* <Text> dsd</Text> */}
+        <View style={styles.calculationsBoard}>
+          <Text style={styles.calcsBoardInput}> {inputedNums}</Text>
+        </View>
+        <View style={styles.resultBoard}>
+          <Text style={styles.calcsBoardInput}> {result}</Text>
+        </View>
+      </View>
       <View style={styles.btnsBoard}>
-        <View style={styles.rowContainer}>
+        <View style={styles.btnsColumnContainer}>
           {/* <Text>T</Text> */}
           {fCol.map((item) => {
-            return <NumberComp style={styles.rowContainer} text={item} />;
+            return (
+              <NumberComp text={item} updateCalcsBoard={updateCalcsBoard} />
+            );
+          })}
+        </View>
+        <View style={styles.btnsColumnContainer}>
+          {/* <Text>T</Text> */}
+
+          {sCol.map((item) => {
+            return (
+              <NumberComp text={item} updateCalcsBoard={updateCalcsBoard} />
+            );
+          })}
+        </View>
+        <View style={styles.btnsColumnContainer}>
+          {/* <Text>T</Text> */}
+          {tCol.map((item) => {
+            return (
+              <NumberComp text={item} updateCalcsBoard={updateCalcsBoard} />
+            );
+          })}
+        </View>
+        <View style={styles.specialBtnsColumnContainer}>
+          {/* <Text>T</Text> */}
+          {lCol.map((item) => {
+            return (
+              <NumberComp text={item} updateCalcsBoard={updateCalcsBoard} />
+            );
+          })}
+        </View>
+      </View>
+    </View>
+  ) : (
+    <View style={lStyles.container}>
+      <View style={lStyles.mathBoard}>
+        <View style={styles.calculationsBoard}>
+          <Text style={styles.calcsBoardInput}> {inputedNums}</Text>
+        </View>
+        <View style={styles.resultBoard}>
+          <Text style={styles.calcsBoardInput}> {result}</Text>
+        </View>
+      </View>
+      <View style={lStyles.btnsBoard}>
+        <View style={lStyles.btnsColumnContainer}>
+          {fCol.map((item) => {
+            return (
+              <NumberComp text={item} updateCalcsBoard={updateCalcsBoard} />
+            );
           })}
         </View>
       </View>
     </View>
   );
+
+  // if (orientation) {
+  //   return (
+  //     <View style={styles.container}>
+  //       <View style={styles.mathBoard}>
+  //         {/* <Text> dsd</Text> */}
+  //         <View style={styles.calculationsBoard}>
+  //           <Text style={styles.calcsBoardInput}> {inputedNums}</Text>
+  //         </View>
+  //         <View style={styles.resultBoard}>
+  //           <Text style={styles.calcsBoardInput}> {result}</Text>
+  //         </View>
+  //       </View>
+  //       <View style={styles.btnsBoard}>
+  //         <View style={styles.btnsColumnContainer}>
+  //           {/* <Text>T</Text> */}
+  //           {fCol.map((item) => {
+  //             return (
+  //               <NumberComp text={item} updateCalcsBoard={updateCalcsBoard} />
+  //             );
+  //           })}
+  //         </View>
+  //         <View style={styles.btnsColumnContainer}>
+  //           {/* <Text>T</Text> */}
+
+  //           {sCol.map((item) => {
+  //             return (
+  //               <NumberComp text={item} updateCalcsBoard={updateCalcsBoard} />
+  //             );
+  //           })}
+  //         </View>
+  //         <View style={styles.btnsColumnContainer}>
+  //           {/* <Text>T</Text> */}
+  //           {tCol.map((item) => {
+  //             return (
+  //               <NumberComp text={item} updateCalcsBoard={updateCalcsBoard} />
+  //             );
+  //           })}
+  //         </View>
+  //         <View style={styles.specialBtnsColumnContainer}>
+  //           {/* <Text>T</Text> */}
+  //           {lCol.map((item) => {
+  //             return (
+  //               <NumberComp text={item} updateCalcsBoard={updateCalcsBoard} />
+  //             );
+  //           })}
+  //         </View>
+  //       </View>
+  //     </View>
+  //   );
+  // } else {
+  //   return (
+  //     <View style={lStyles.container}>
+  //       <View style={lStyles.mathBoard}>
+  //         <View style={styles.calculationsBoard}>
+  //           <Text style={styles.calcsBoardInput}> {inputedNums}</Text>
+  //         </View>
+  //         <View style={styles.resultBoard}>
+  //           <Text style={styles.calcsBoardInput}> {result}</Text>
+  //         </View>
+  //       </View>
+  //       <View style={lStyles.btnsBoard}>
+  //         <View style={styles.btnsColumnContainer}>
+  //           <Text>T</Text>
+  //           {fCol.map((item) => {
+  //             return (
+  //               <NumberComp text={item} updateCalcsBoard={updateCalcsBoard} />
+  //             );
+  //           })}
+  //         </View>
+  //       </View>
+  //     </View>
+  //   );
+  // }
 };
 
 const styles = StyleSheet.create({
@@ -28,12 +199,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#ffff00",
   },
-  rowContainer: {
+  btnsColumnContainer: {
     flex: 1,
+    // padding: 30,
+    // paddingTop: 35,
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+  specialBtnsColumnContainer: {
+    flex: 1,
+    // padding: 30,
+    // paddingTop: 25,
+    backgroundColor: "#bbb",
+    // justifyContent: "center",
+    // alignItems: "center",
   },
   mathBoard: {
     flex: 1,
-    flexDirection: "row",
+    flexDirection: "column",
     backgroundColor: "#00fff0",
   },
   btnsBoard: {
@@ -44,6 +227,41 @@ const styles = StyleSheet.create({
   reversedContainer: {
     flex: 1,
     flexDirection: "column-reverse",
+  },
+  calculationsBoard: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    paddingRight: 10,
+  },
+  calcsBoardInput: {
+    fontSize: 48,
+  },
+  resultBoard: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    paddingRight: 10,
+  },
+});
+
+const lStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffff00",
+  },
+  mathBoard: {
+    flex: 4,
+    flexDirection: "column",
+    backgroundColor: "#00fff0",
+  },
+  btnsBoard: {
+    flex: 7,
+    flexDirection: "row",
+    backgroundColor: "#666",
+  },
+  btnsColumnContainer: {
+    flex: 1,
   },
 });
 
--- components/NumberComp.js
+++ components/NumberComp.js
@@ -1,23 +1,81 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import React, { useState } from "react";
 
-const NumberComp = (props) => {
-  return (
-    <View style={styles.buttonContainer}>
-      <Text style={styles.text}>{props.text}</Text>
-    </View>
-  )
-}
+const NumberComp = ({ text, updateCalcsBoard }) => {
+  const [orientation, setOrientation] = useState(true);
 
-const styles = StyleSheet.create({
-    buttonContainer: {
-        flex: 1
-    },
- text: {
-    fontSize: 40,
-    color: "#eee"
- }
+  const isPortrait = () => {
+    let dim = Dimensions.get("screen");
+    console.log("robie i zwaracam " + dim);
+    return dim.height >= dim.width;
+  };
+
+  Dimensions.addEventListener("change", () => {
+    setOrientation(isPortrait());
   });
 
+  const handleClick = () => {
+    updateCalcsBoard(text);
 
-export default NumberComp
+    // if (text != "C" && text != "=") {
+    //   // console.log("test");
+    //   updateCalcsBoard(text);
+    // }
+    // else if( text == "="){
+    //   eval
+    // }
+  };
+  return orientation ? (
+    <TouchableOpacity onPress={handleClick} style={styles.touchStyle}>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.text}>{text}</Text>
+      </View>
+    </TouchableOpacity>
+  ) : (
+    <TouchableOpacity onPress={handleClick} style={styles.touchStyle}>
+      <View style={lStyles.buttonContainer}>
+        <Text style={styles.text}>{text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20,
+    paddingTop: 30,
+  },
+  text: {
+    fontSize: 42,
+    color: "#eee",
+  },
+  touchStyle: {
+    flex: 1,
+  },
+});
+
+const lStyles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    alignItems: "center",
+    // padding: 20,
+    // paddingTop: 30,
+  },
+  text: {
+    fontSize: 42,
+    color: "#eee",
+  },
+  touchStyle: {
+    flex: 1,
+  },
+});
+
+export default NumberComp;
